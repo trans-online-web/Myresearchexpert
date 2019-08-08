e0c73e2338d55e3afd9ae4d7a53f802c834db8d2
Ochieng424 <ochiengderrick424@gmail.com>
2019-08-08 13:10:43 +0300

    created tasks migration

---
+++ database/migrations/2019_08_08_093655_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->references('id')->on('users');
+            $table->integer('subject_id')->references('id')->on('subjects');
+            $table->integer('documentType_id')->references('id')->on('documentTypes');
+            $table->date('deadline_date');
+            $table->time('deadline_time');
+            $table->string('level');
+            $table->string('title');
+            $table->longText('task');
+            $table->integer('pages');
+            $table->integer('spacing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
