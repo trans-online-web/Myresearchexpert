b2fa7c692cd5b178ad1aa712b3e6fb124a65e68f
shemnduati <nduatishem@gmail.com>
2019-08-20 18:40:11 +0300

    added laravel pasport for authentication

--- app/Http/Controllers/API/SubjectController.php
+++ app/Http/Controllers/API/SubjectController.php
@@ -9,7 +9,10 @@ use App\Http\Controllers\Controller;
 
 class SubjectController extends Controller
 {
-
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     /**
      * Display a listing of the resource.
      *
