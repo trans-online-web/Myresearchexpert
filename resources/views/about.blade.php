a7b10074d7b6142db4caa98b0e0dda780dc4efe5
engnr <samsrut@gmail.com>
2019-10-08 09:51:47 +0300

    partial content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,12 +6,7 @@
     <div class="row h-100 align-items-center">
       <div class="col-12">
         <h1 class="font-weight-light">About Us</h1>
-        <p class="lead3">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="lead3">We always advice you to choose the right research paper writing service: Many students end up losing their money for not making the right choice whom to write their papers. We guarantee high quality paper and timely submissions. Contact us to place your order.</p>
       </div>
     </div>
   </div>
@@ -24,20 +19,20 @@
 			</div>
 		</div>
 		<div class="card col-md-6">
-			<h2 class="header font-weight-bold p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+			<h2 class="header font-weight-bold p-2">Why order research paper writing from us.</h2>
 			<div class="steps">
 				<ul>
 					<li>
-						<h4 class="featurette pl-3">Lorem ipsum dolor sit amet</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?Lorem ipsum dolor sit amet, </p>
+						<h4 class="featurette pl-3">Quality work</h4>
+						<p>Our company is made up of a huge team of highly qualified writers from different fields with firsthand experience on handling research papers. Such a team is dedicated to offer nothing short of quality. Each paper is done from scratch through extensive research on the topic. Moreover, our writers guarantee a paper free of grammar mistakes. With us you can therefore bet on best research paper.</p>
 					</li>
 					<li>
-						<h4 class="featurette pl-3">Lorem ipsum dolor sit amet</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?Lorem ipsum dolor sit amet,</p>
+						<h4 class="featurette pl-3">Timely delivery</h4>
+						<p>Our company is time conscious, submit an order to us if you need help writing research paper so as to avoid last minute panic or delayed delivery. All our writers are disciplined individuals who are very time conscious. We believe in working on a client’s work immediately and strive to submit it as early as possible. Most of our orders are submitted before the stipulated time. </p>
 					</li>
 					<li>
-						<h4 class="featurette pl-3">Lorem ipsum dolor sit amet</h4>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?Lorem ipsum dolor sit amet,</p>
+						<h4 class="featurette pl-3">Originality</h4>
+						<p>All our orders produces non-plagiarized research paper, this is so because we start each item from scratch following the order requirements and client preferences. Our writers conduct extensive research on the given topic and gather sufficient content, including evidence to include in your paper.</p>
 					</li>
 				</ul>
 			</div>
@@ -47,13 +42,13 @@
 
 <section class="about_p2">
 	<div class="row justify-content-center py-5">
-		<h2 class="text-center pt-5 featurette">Lorem ipsum dolor sit amet, consectetur Quo.</h2>
+		<h2 class="text-center pt-5 featurette">Can I get research paper writing?</h2>
 		<div class="row px-5 pt-3">
 			<div class="col-md-6 lead2">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?
+				Definitely yes! As an esteemed client, be assured that we have the research paper writing help anytime and the selected professional writer will be assigned immediately and proceed to produce a quality research paper that will meet your demands.
 			</div>
 			<div class="col-md-6 lead2">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam accusantium voluptatum doloremque quibusdam veritatis accusamus adipisci dolores error saepe, esse odit sed, fugit laudantium corporis culpa harum, atque ratione! Nulla?
+				Students who have tried our services were satisfied beyond their expectations and have taken an initiative to recommend our services to their friends. After using our research paper writing service, do not hesitate to recommend us to a friend, and you will get a discount.
 			</div>
 		</div>
 	</div>
