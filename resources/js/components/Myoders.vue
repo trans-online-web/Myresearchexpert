33477a20f023ef1f2f6441b60d54db2c34982a0f
shemnduati <nduatishem@gmail.com>
2019-10-23 13:49:18 +0300

    changes on the dashboard

--- resources/js/components/Myoders.vue
+++ resources/js/components/Myoders.vue
@@ -39,7 +39,7 @@
                                     </td>
                                     <td><i class="fa fa-clock-o mr-1"></i>{{order.deadline_datetime | myDate}}</td>
                                     <td>
-                                        <a href="/myorderdetails/1" type="button" class="btn btn-primary btn-sm">More</a>
+                                        <a :href="'/myorderdetails/'+ order.id" type="button" class="btn btn-primary btn-sm">More</a>
                                     </td>
                                     <td>
                                         <a href="#" @click="editModal(order, order.id)">
